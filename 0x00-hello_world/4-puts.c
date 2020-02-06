78d384a1024c174c096d84e15ed2a4b007b5de11
Kelsie Merchant <eislek02@gmail.com>
2020-02-06 01:53:01 +0000

    Update code to run C program with correct end value of 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include <string.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	getchar();
+	char string[52];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+	puts(string);
 	return (0);
 }
