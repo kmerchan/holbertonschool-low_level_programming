9ef13aaf5dcefca3e145f5d1456b937667229066
Kelsie Merchant <eislek02@gmail.com>
2020-02-06 22:25:21 -0600

    Add C source code to print 0-9separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
