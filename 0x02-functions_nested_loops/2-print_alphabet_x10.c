78116a0e2f8025406eb744aa45eb1a162455a9ad
Kelsie Merchant <eislek02@gmail.com>
2020-02-10 23:52:50 +0000

    Add C source code file for function to print alphabet 10 times and update header file with function prototype

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * function will print the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet_x10(void)
+{
+	int n;
+	int t;
+
+	/* loop through 10 times */
+	for (t = 0; t <= 9; t++)
+	{
+		/* 97 is ASCII value for 'a' */
+		n = 97;
+		/* loop through from 97 ('a') t0 122 ('z') */
+		while (n <= 122)
+		{
+			_putchar(n);
+			n++;
+		}
+		/* Print new line after alphabet */
+		_putchar(10);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,3 +3,6 @@ int _putchar(char c);
 
 /* declaration for function to print alphabet */
 int print_alphabet(void);
+
+/* declaration for function to print alphabet 10 times */
+int print_alphabet_x10(void);
