bd5cd7a5c06d16fdbe26c4e5362ccc5bfa9936e7
Kelsie Merchant <eislek02@gmail.com>
2020-02-11 10:28:34 -0600

    Update file to loop through string without printing null character

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	/* Create array of characters to spell Holberton */
-	char array[10] = "Holberton";
+	char array[9] = "Holberton";
 	int index;
 
 	/* Loop through array to print full string */
-	while (index < 10)
+	while (index < 9)
 	{
 		_putchar(array[index]);
 		index++;
