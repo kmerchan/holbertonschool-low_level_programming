9b0eea55a0500d17e8032e4b51b25968635c1ddb
Kelsie Merchant <eislek02@gmail.com>
2020-02-12 00:00:07 -0600

    Update to include three digit numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,15 +7,9 @@
 
 void print_times_table(int n)
 {
-	int side;
-	int top;
-	int x;
+	int side, top, x;
 
-	if (n > 15 || n < 0)
-	{
-		_putchar(10);
-	}
-	else
+	if (n < 15 || n > 0)
 	{
 		for (side = 0; side <= n; side++)
 		{
@@ -25,13 +19,30 @@ void print_times_table(int n)
 				x = side * top;
 				_putchar(44);
 				_putchar(32);
-				if ((x / 10) == 0)
+				if ((x / 100) == 0 && (x / 10) == 0)
 				{
 					_putchar(32);
-					_putchar(x + '0');
+					_putchar(32);
+					_putchar((x % 10) + '0');
+				}
+				else if ((x / 100) == 0)
+				{
+					if ((x / 10) == 0)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar((x % 10) + '0');
+					}
+					else
+					{
+						_putchar(32);
+						_putchar((x / 10) + '0');
+						_putchar((x % 10) + '0');
+					}
 				}
 				else
 				{
+					_putchar((x / 100) + '0');
 					_putchar((x / 10) + '0');
 					_putchar((x % 10) + '0');
 				}
