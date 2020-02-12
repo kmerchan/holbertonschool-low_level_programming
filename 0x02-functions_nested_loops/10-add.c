58c82ad888bd31a1f3101ff4846b524730f0189c
Kelsie Merchant <eislek@gmail.com>
2020-02-12 00:23:08 +0000

    Add C source code file to define function to return the sum of two integers and update header file to include function prototype

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers together
+ * @a: first input integer to add
+ * @b: second input integer to add
+ *
+ * Return: integer sum of a + b
+ */
+
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,29 +1,32 @@
-/* declaration for printing character */
+/* declaration for printing character (0)*/
 int _putchar(char c);
 
-/* declaration for function to print alphabet */
+/* declaration for function to print alphabet (1)*/
 void print_alphabet(void);
 
-/* declaration for function to print alphabet 10 times */
+/* declaration for function to print alphabet 10 times (2)*/
 void print_alphabet_x10(void);
 
-/* declaration for function to check for lowercase character */
+/* declaration for function to check for lowercase character (3)*/
 int _islower(int c);
 
-/* declaration for function to check if alpha character */
+/* declaration for function to check if alpha character (4)*/
 int _isalpha(int c);
 
-/* declaration for function to print sign of int n */
+/* declaration for function to print sign of int n (5)*/
 int print_sign(int n);
 
-/* declaration for function to return absolute value of int */
+/* declaration for function to return absolute value of int (6)*/
 int _abs(int);
 
-/* declaration for function to return value of last digit */
+/* declaration for function to return value of last digit (7)*/
 int print_last_digit(int);
 
-/* declaration for function to print every minute of the day */
+/* declaration for function to print every minute of the day (8)*/
 void jack_bauer(void);
 
-/* declaration for function to print 9x9 times table */
+/* declaration for function to print 9x9 times table (9)*/
 void times_table(void);
+
+/* declaration for function to add two integers together (10)*/
+int add(int, int);
