cea4ec3af5dc019688bb0c4a60ce0466c6c68c43
Kelsie Merchant <eislek@gmail.com>
2020-02-13 22:42:47 +0000

    Initial commit of file to start function for FizzBuzz interview question

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 1-100 where multiples of 3 and/or 5 print words
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	/* c = 65 is 'A' and c = 90 is 'Z' */
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
