35c282ee94b93a8f2372c77f77e4d861c6c10a1e
Kelsie Merchant <eislek02@gmail.com>
2020-02-17 22:58:59 -0600

    Update header file with all function prototypes

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,32 @@
+/* decalration of function to print character (_putchar)*/
+int _putchar(char c);
+
 /* declaration of function to take pointer and update so it points to 98 (0)*/
 void reset_to_98(int *n);
 
 /* declaration of function to swap values of two integers with pointers (1)*/
 void swap_int(int *a, int *b);
+
+/* declaration of function to return length of a string (2)*/
+int _strlen(char *s);
+
+/* declaration of function to print a string (3)*/
+void _puts(char *str);
+
+/* declaration of function to print string in reverse order (4)*/
+void print_rev(char *s);
+
+/* declaration of function to reverse a string (5)*/
+void rev_string(char *s);
+
+/* declaration of function to print every other character of a string (6)*/
+void puts2(char *str);
+
+/* declaration of function to print second half of string (7)*/
+void puts_half(char *str);
+
+/* declaration of function to print n elements of array (8)*/
+void print_array(int *a, int n);
+
+/* declaration of function to copy string, including null byte (9)*/
+char *_strcopy(char *dest, char *src);
