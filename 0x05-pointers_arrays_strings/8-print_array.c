b3a7a998910a2475b30fb804c2929fc937786d6d
Kelsie Merchant <eislek02@gmail.com>
2020-02-18 00:02:59 -0600

    Add C source code file to define function to print n elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints nth elements of an array
+ * @n: input number of array elements to print
+ *
+ * @a: integer value of array
+ *
+ * @*a: input pointer to array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
