4c56164f6a31a68b74fc6a2ca0171792d4091978
Kelsie Merchant <eislek02@gmail.com>
2020-02-20 13:48:49 -0600

    Add C source code to define function to copy string src up to n bytes to string dest

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - copies src string up to n bytes to dest string
+ * @src: input source string to copy to dest
+ *
+ * @dest: input destination string to copy to
+ *
+ * @n: input number of bytes to append
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
