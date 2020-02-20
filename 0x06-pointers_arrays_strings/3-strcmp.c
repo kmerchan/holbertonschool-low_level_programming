ec43f188cab07a8c1fff7e73b6e877604e49fc07
Kelsie Merchant <eislek02@gmail.com>
2020-02-20 14:39:51 -0600

    Initial commit of function to compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: input string 1
+ *
+ * @s2: input string 2
+ *
+ * Return: integer
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int i, r;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if 
+		i++;
+	}
+	return (r);
+}
