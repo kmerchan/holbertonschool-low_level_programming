9120687f25ac80b88989ce6f64833ff974247068
Kelsie Merchant <eislek@gmail.com>
2020-02-20 17:29:42 +0000

    Add C source code file to define function to concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - concatenates two strings/appends src string to dest string
+ * @src: input source string to append to dest
+ *
+ * @dest: input destination string to append to
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
