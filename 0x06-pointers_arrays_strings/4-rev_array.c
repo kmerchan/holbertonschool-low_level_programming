d7d1610413f915b506e5ee023e059e4a936aa1e5
Kelsie Merchant <eislek@gmail.com>
2020-02-20 21:59:29 +0000

    Add C source code file to define function to reverse array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+ * reverse_array - reverses an array of integers
+ * @a: input array of integers
+ *
+ * @n: number of elements in array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, holder, end;
+
+	end = n - 1;
+	for (i = 0; i <= (n / 2); i++, end--)
+	{
+		holder = a[i];
+		a[i] = a[end];
+		a[end] = holder;
+	}
+}
