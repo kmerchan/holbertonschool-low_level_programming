e8715015f7034e48ec427a6635a30dc548170e0a
Kelsie Merchant <eislek02@gmail.com>
2020-02-20 13:58:26 -0600

    Fix code to add null character after copied string until n bytes are reached

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,6 +19,14 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
+		if (src[i] == '\0')
+		{
+			while (i < n)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
 	}
 	return (dest);
 }
