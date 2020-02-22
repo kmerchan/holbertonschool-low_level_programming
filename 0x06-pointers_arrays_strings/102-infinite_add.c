b1ac4a644258c9d783a75d2fb921af1bc99b6428
Kelsie Merchant <eislek02@gmail.com>
2020-02-21 21:23:48 -0600

    Update C source code file to define funciton to change strings to integers, add, and return as string

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,6 +1,33 @@
 #include "holberton.h"
 
 /**
+ * a_to_int - get integer value of string of characters
+ * @s: string to change to integer
+ *
+ * Return: integer value of string
+ */
+
+int a_to_int(char *s)
+{
+	int i = 0;
+	int size = 0;
+	int place = 1;
+	int num = 0;
+
+	while (s[i])
+	{
+		size++;
+		i++;
+	}
+	for (i = (size - 1); i >= 0; i--)
+	{
+		num += ((s[i] - '0') * place);
+		place *= 10;
+	}
+	return (num);
+}
+
+/**
  * infinite_add - adds two numbers
  * @n1: first input number
  *
@@ -10,37 +37,36 @@
  *
  * @size_r: size of buffer
  *
- * Return: pointer to buffer
+ * Return: pointer to buffer or 0 if size of result is greater than buffer size
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, size;
+	int num1, num2, sum, n, digits, i;
 
-	size = 0;
+	num1 = a_to_int(n1);
+	num2 = a_to_int(n2);
+	sum = num1 + num2;
+	n = sum;
+	digits = 1;
+	i = 0;
 
-	for (i = 0; n1[i] != 0 && n2[i] != 0; i++)
+	while ((n / 10) != 0)
 	{
-		size++;
-		r[i] = 0;
+		digits++;
+		n /= 10;
 	}
-	if (size > size_r)
+	if ((digits + 1) > size_r)
 	{
 		return (0);
 	}
 	else
 	{
-		for (i = 0; n1[i] != 0 || n2[i] != 0; i++)
+		r[digits] = '\0';
+		for (i = (digits - 1); i >= 0; i--)
 		{
-			if ((n1[i] + n2[i]) > 9)
-			{
-				r[i] = r[i] + (n1[i] + n2[i]) % 10;
-				r[i + 1] = r[i] + (n1[i] + n2[i]) / 10;
-			}
-			else
-			{
-				r[i] = r[i] + n1[i] + n2[i];
-			}
+			r[i] = (sum % 10) + '0';
+			sum /= 10;
 		}
 	}
 	return (r);
