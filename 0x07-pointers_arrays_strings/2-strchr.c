8f41cc01614ed90870c2ce063582e67e23bd4753
Kelsie Merchant <eislek@gmail.com>
2020-02-24 16:44:34 +0000

    Add C source code file to define function to find first location of character in string or null if character is not in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - finds character in a string and returns address of that character
+ * @s: input pointer to string of characters
+ *
+ * @c: input character to find
+ *
+ * Return: pointer to first occurance of char c in string or null if no char c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		return (&s[i]);
+	}
+}
