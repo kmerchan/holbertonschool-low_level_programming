e919121dbe3987a239394071a611095eb7d8def5
Kelsie Merchant <eislek@gmail.com>
2020-02-24 22:00:17 +0000

    Add C source code file to define function to locate substring inside string of characters or return null if substring does not exist in string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+/**
+ * _strstr - finds matching set of bytes of one substring in another string
+ * @haystack: input pointer to string of characters to check
+ *
+ * @needle: input pointer to substring of characters to look for
+ *
+ * Return: pointer to location of substring or null if substring not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int size = 0;
+	int x = 0;
+
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		size++;
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			x = 0;
+			while (x < size && haystack[i + x] == needle[x])
+			{
+				if (x == (size - 1))
+				{
+					return (&haystack[i]);
+				}
+				x++;
+			}
+			if (haystack[i + x] == '\0' && needle[x] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	if (haystack[i] == '\0' && needle[0] == '\0')
+	{
+		return (&haystack[i]);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
