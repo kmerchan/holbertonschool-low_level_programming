ea36f1247e2510c2b2e66807cc5e4ae6b8055c95
Kelsie Merchant <eislek@gmail.com>
2020-02-24 20:12:55 +0000

    Add C source code file to test peer's code with slight adjustment against the checker

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,29 +9,34 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
-	int size = 0;
-	unsigned int count = 0;
+	unsigned int i;
+	unsigned int j, k, m;
+	m = 0;
 
-	for (j = 0; accept[j] != '\0'; j++)
-	{
-		size++;
-	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				count++;
+				k++;
 				break;
 			}
-			else if (j == (size - 1) && s[i] != accept[j])
+			else
+				k = 0;
+		}
+		if (k == 0)
+		{
+			break;
+		}
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
 			{
-				return (count);
+				m++;
+				break;
 			}
 		}
 	}
-	return (count);
+	return (m);
 }
