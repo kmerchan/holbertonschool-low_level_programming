bb6c3142cdcf2647f62ecdc2069ef7833576061b
Kelsie Merchant <eislek@gmail.com>
2020-02-25 03:04:14 +0000

    Add peer's C source code file with slight adjustment to check against checker

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,48 +1,46 @@
+ #include "holberton.h"
 /**
- * _strstr - finds matching set of bytes of one substring in another string
- * @haystack: input pointer to string of characters to check
+ * _strstr - find a string within a string
  *
- * @needle: input pointer to substring of characters to look for
+ * @haystack: the string we are looking into to see if there is a match
+ * to out smaller string, needle.
  *
- * Return: pointer to location of substring or null if substring not found
+ * @needle: out key string that we are comparing against haystack
+ *
+ * Return: return the location of where the match to need starts
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 	int j = 0;
-	int size = 0;
-	int x = 0;
-
-	for (j = 0; needle[j] != '\0'; j++)
+	char *point = 0;
+	if (needle[0] == '\0')
 	{
-		size++;
+		return (&haystack[0]);
 	}
-	if (size == 0)
+	while (haystack[i] != '\0')
 	{
-		return (haystack);
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[0])
+		if (haystack[i] == needle[j])
 		{
-			x = 0;
-			while (x < size && haystack[i + x] == needle[x])
+			point = &haystack[i];
+			for (j = 1; needle[j] != '\0'; j++)
 			{
-				if (x == (size - 1))
+				i += 1;
+				if (needle[j] == haystack[i])
 				{
-					return (&haystack[i]);
+					if (needle[j + 1] == '\0')
+						return (point);
 				}
-				x++;
+				else
+				{
+					j = 0;
+					break;
+				}
 			}
 		}
+		else
+			i++;
 	}
-	if (haystack[i] == '\0' && needle[x] == '\0')
-	{
-		return (&haystack[i]);
-	}
-	else
-	{
-		return ('\0');
-	}
+	point = 0;
+	return (point);
 }
