0eef356e1dc6a3d0ac8b1edc6d24556278cf98a5
Kelsie Merchant <eislek@gmail.com>
2020-02-25 01:08:33 +0000

    Re-add my C source code file to define function to find first occurance of character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,23 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != c && s[i] != '\0')
 	{
-		if (s[i] == c)
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		if (c == '\0')
 		{
-			s = s + i;
-			return (s);
+			return (&s[i]);
+		}
+		else
+		{
+			return ('\0');
 		}
 	}
-
-	if (c == '\0')
-		s = s + i;
 	else
-		s = 0;
-	return (s);
+	{
+		return (&s[i]);
+	}
 }
