1b94c8fecb7cb70cab90bc11f0801058fa0b054c
Kelsie Merchant <eislek@gmail.com>
2020-02-26 20:22:59 +0000

    Update header file with all mandatory task function prototypes

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,3 +9,18 @@ void _print_rev_recursion(char *s);
 
 /* declaration of function to return length of string (2)*/
 int _strlen_recursion(char *s);
+
+/* declaration of function to return factorial of number (3)*/
+int factorial(int n);
+
+/* declaration of function to return value of x raised to the power of y (4)*/
+int _pow_recursion(int x, int y);
+
+/* declaration of function to return natural square root of a number (5)*/
+int _sqrt_recursion(int n);
+
+/* declaration of function to determine if a number is prime (6)*/
+int is_prime_number(int n);
+
+/* declaration of function to determine if a string is a palindrome (7)*/
+int is_palindrome(char *s);
