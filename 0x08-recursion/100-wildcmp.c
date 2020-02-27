3710e2218d9586f0f269e55eebc0048ed6108a22
Kelsie Merchant <eislek02@gmail.com>
2020-02-27 00:04:53 -0600

    Update

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -11,49 +11,15 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	return (stringcmp(s1, s2, 0));
-}
-
-/**
- * stringcmp - function to compare two strings
- * @s1: input string s1
- *
- * @s2: input string s2
- *
- * @checker: checker to determine if wildcard has been seen
- *
- * Return: 1 if match, 0 if don't
- */
-
-int stringcmp(char *s1, char *s2, int checker)
-{
-	int count;
-
 	if (s2[0] == '*')
 	{
-		checker++;
 		s2 = wilds2(s2);
 		s1 = wilds1(s1, s2[0]);
-		return (stringcmp(s1, s2, checker));
+		return (wildcmp(s1, s2));
 	}
 	else if (s1[0] != s2[0])
 	{
-		if (checker > 0)
-		{
-			count = checkwd(s1, s2, 0);
-			s1 -= count;
-			s1 += 2;
-			if (s1[0] == '\0')
-			{
-				return (0);
-			}
-			s2 -= count;
-			return (stringcmp(s1, s2, checker));
-		}
-		else
-		{
-			return (0);
-		}
+		return (0);
 	}
 	else if (s1[0] == '\0' && s2[0] == '\0')
 	{
@@ -63,11 +29,10 @@ int stringcmp(char *s1, char *s2, int checker)
 	{
 		s1++;
 		s2++;
-		return (stringcmp(s1, s2, checker));
+		return (wildcmp(s1, s2));
 	}
 	return (0);
 }
-
 
 /**
  * wilds2 - function to handle wildcards in string 2
@@ -95,7 +60,6 @@ char *wilds2(char *s2)
  * @s1: input string s1
  *
  * @next: input of what next character of s2 is
- *
  * Return: start of next character of s1
  */
 
@@ -105,41 +69,10 @@ char *wilds1(char *s1, char next)
 	{
 		return (s1);
 	}
-	else if (s1[0] == '\0')
-	{
-		return (s1);
-	}
 	else
 	{
 		s1++;
 		return (wilds1(s1, next));
-	}
-	return (0);
-}
-
-/**
- * checkwd - function to check if s2 has seen a wildcard before non-match
- * @s1: input string s1
- *
- * @s2: input string s2
- *
- * @count: count of how many matching places to get back to wildcard
- *
- * Return: 0 if no wildcards previously and 1 if has seen wildcard
- */
-
-int checkwd(char *s1, char *s2, int count)
-{
-	if (s2[0] == '*')
-	{
-		return (count);
-	}
-	else
-	{
-		count++;
-		s2--;
-		s1--;
-		return (checkwd(s1, s2, count));
 	}
 	return (0);
 }
