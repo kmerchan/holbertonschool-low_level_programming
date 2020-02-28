55356357552f163f6b597c94ea9382b305d69118
Kelsie Merchant <eislek02@gmail.com>
2020-02-28 22:13:31 +0000

    Initial commit of directory for argc and argv work

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - function to print name of function followed by newline
+*
+* Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
