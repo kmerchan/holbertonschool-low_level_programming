8af479f465faf363a048ad6bc09ea3219bdb9dc0
Kelsie Merchant <eislek02@gmail.com>
2020-02-29 15:25:44 -0600

    Update code to describe arguments in function comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,9 @@
 
 /**
 * main - function to print name of function followed by newline
+* @argc: unused argument
+*
+* @argv: argument whos first element [0] is the program name
 *
 * Return: (0)
 */
@@ -11,5 +14,5 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
