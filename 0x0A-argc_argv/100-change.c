8c47d5e77318c8bf2a5feb02a601a0be028390d5
Kelsie Merchant <eislek02@gmail.com>
2020-03-01 00:44:22 -0600

    Initial commit of C source code file to determine minimum number of coins to make change, where change is the argument passed to a program

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - function to determine minimum number of coins needed to make change
+* @argc: argument to determine how many arguments to program are provided
+*
+* @argv: argument array with program name and arguments
+*
+* Return: (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	int change;
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	change = atoi(argv[1]);
+
+	if (change < 0)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		coins += divide_change(change, 25);
+		coins += divide_change(change, 10);
+		coins += divide_change(change, 5);
+		coins += divide_change(change, 2);
+		coins += divide_change(change, 1);
+		printf("%d\n", coins);
+	}
+	return (0);
+}
+
+/**
+* divide_change - subtracts off change while still divisible by cent amount
+* @change: total change left to make
+*
+* @cent: largest cent to make change with
+*
+* Return: coins needed to make change with that cent
+*/
+
+int divide_change(int change, int cent)
+{
+	int coins = 0;
+
+	while (change % cent == 0)
+	{
+		coins++;
+		change -= cent;
+	}
+	return (coins);
+}
