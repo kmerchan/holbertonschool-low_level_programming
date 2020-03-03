78264a399d41b4f22ceb06b399fdf06d955491a6
Kelsie Merchant <eislek02@gmail.com>
2020-03-03 13:02:49 -0600

    Update code to return null if malloc does not have enough space in memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = (char *) malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || array == '\0')
 	{
 		return (0);
 	}
