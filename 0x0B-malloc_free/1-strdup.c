165cf044fdf2c51e811cec8bae92e220fa3799a8
Kelsie Merchant <eislek02@gmail.com>
2020-03-03 13:12:11 -0600

    Add C source code file to define function to create new pointer in memory that points to copy of a string and update header file to include function prototype

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to new allocated space with copy of string
+ * @str: input string to copy
+ *
+ * Return: pointer to newly allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int i = 0;
+
+	if (str == '\0')
+	{
+		return (0);
+	}
+	for (i = 0; str[i]; i++);
+	array = (char *) malloc((sizeof(char) * i) + 1);
+
+	if (array == '\0')
+	{
+		return (0);
+	}
+	array[i + 1] = '\0';
+	for (; i >= 0; i--)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,2 +1,5 @@
 /* declaration of function to create and initialize character array (0)*/
 char *create_array(unsigned int size, char c);
+
+/*declaration of function to return new pointer containing copy of string (1)*/
+char *_strdup(char *str);
