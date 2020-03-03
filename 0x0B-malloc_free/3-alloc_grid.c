aa01c4f6528308bff5a32e6b6870b781989ef152
Kelsie Merchant <eislek02@gmail.com>
2020-03-03 17:56:28 -0600

    Initial commit of function to initialize 2-D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to two-dimensional int array
+ * @width: input number of columns in 2-D array
+ *
+ * @height: input number of rows in 2-D array
+ *
+ * Return: pointer to newly allocated memory with 2-D int array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	char **array;
+	int rows, columns, x;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+	array = malloc(height * sizeof(int));
+	if (array == '\0')
+	{
+		return (0);
+	}
+	for (rows = 0; rows < height; rows++)
+	{
+		array[rows] = malloc(width * sizeof(int));
+		if (array[rows] == '\0')
+		{
+			return (0);
+		}
+	}
+	for (rows = 0; rows < height; rows++)
+	{
+		for (columns = 0; columns < width; columns++)
+		{
+			x = (width * rows) + columns;
+			*(array + x) = 0;
+		}
+	}
+	return (array);
+}
