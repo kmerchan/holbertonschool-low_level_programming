6db340995fb7e4338b02c7c9bac4e77665491efe
Kelsie Merchant <eislek02@gmail.com>
2020-03-03 12:32:28 -0600

    Update C source file to move size of array to where array is initialized

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *array;
+	char *array = malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
 		return (0);
 	}
-	array = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 	{
