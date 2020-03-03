a98daa22e953696e175ef9c45d115567fd213781
Kelsie Merchant <eislek02@gmail.com>
2020-03-03 12:11:49 -0600

    Add C source code file to create character array of specified size with malloc and initialize with given character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create array of characters, initilize with specific character
+ * @size: input size of array
+ *
+ * @c: input character to initialize with
+ *
+ * Return: initialized character array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	array = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
