c33a4342898c6650268d6a7078da0c346de8f843
Kelsie Merchant <eislek02@gmail.com>
2020-03-04 06:06:08 +0000

    Initial commit of idea for advanced task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - function to split string into words
+ * @str: input string of words
+ *
+ * Return: double pointer to array of words
+ */
+
+char **strtow(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int index = 0;
+	int words = 1;
+	char **array;
+
+	if (str == 0)
+		return (0);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ')
+			words++;
+	}
+	array = (char **) malloc(words * sizeof(char *));
+	if (array == '\0')
+		return (0);
+	for (i = 0; i < words; i++)
+	{
+		for (j = 0; str[j] != ' '; j++)
+			continue;
+		j++;
+		array[i] = (char *) malloc((j * sizeof(char)) + 1);
+		if (array[i] == '\0')
+		{
+			for (; i >= 0; i++)
+				free(array[i]);
+			free(array);
+			return (0);
+		}
+	}
+	for (i = 0; i < words; i++)
+	{
+		for (j = 0; str[j] != ' '; j++, index++)
+			array[i][index] = str[j];
+		array[i][index] = '\0';
+		index++;
+	}
+	return (array);
+}
