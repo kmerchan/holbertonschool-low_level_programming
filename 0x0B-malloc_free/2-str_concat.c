8fd56dbb18bc4a10514440fc8d9efe3640165b34
Kelsie Merchant <eislek02@gmail.com>
2020-03-04 05:19:38 +0000

    Update code to check if both strings are null and, if so, to return empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,14 +6,14 @@
  * @s1: input string 1 to copy
  *
  * @s2: input string 2 to copy after string 1
+ *
  * Return: pointer to newly allocated memory with concatenated copy of strings
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *array;
-	int i = 0;
-	int j = 0;
+	int i, j;
 
 	if (s1 != '\0' && s2 != '\0')
 	{
@@ -24,31 +24,32 @@ char *str_concat(char *s1, char *s2)
 	}
 	else if (s1 == '\0')
 	{
-		s1 = malloc(1);
+		s1 = (char *) malloc(1);
 		s1[0] = '\0';
 		for (i = 0; s2[i]; i++)
 			continue;
 	}
 	else if (s2 == '\0')
 	{
-		s2 = malloc(1);
+		s2 = (char *) malloc(1);
 		s2[0] = '\0';
 		for (i = 0; s1[i]; i++)
 			continue;
 	}
+	else
+	{
+		array = (char *) malloc(sizeof(char));
+		array[0] = '\0';
+		return (array);
+	}
 	array = (char *) malloc((sizeof(char) * i) + 1);
-
 	if (array == '\0')
 		return (0);
 	array[i + 1] = '\0';
 	i = 0;
 	for (i = 0; s1[i]; i++)
-	{
 		array[i] = s1[i];
-	}
 	for (j = 0; s2[j]; i++, j++)
-	{
 		array[i] = s2[j];
-	}
 	return (array);
 }
