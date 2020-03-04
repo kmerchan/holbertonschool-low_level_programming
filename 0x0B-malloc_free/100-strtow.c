ad6c2d270a36c08d294e3f4f22931dd04e47394e
Kelsie Merchant <eislek02@gmail.com>
2020-03-04 05:50:58 +0000

    Initial commit of file minus extraneous code

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -10,43 +10,9 @@
 
 char **strtow(char *str)
 {
-	int i = 0;
-	int j = 0;
-	int index = 0;
-	int length = 0;
-	char *array;
+	char *array = str;
 
-	if (ac == 0)
+	if (str == '\0')
 		return (0);
-	for (i = 0; i < ac; i++)
-	{
-		if (av[i] == '\0')
-			return (0);
-		for (j = 0; av[i][j]; j++)
-			continue;
-		length += j + 1;
-	}
-	array = (char *) malloc((length * sizeof(char)) + 1);
-	if (array == '\0')
-	{
-		return (0);
-	}
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++, index++)
-		{
-			array[index] = av[i][j];
-		}
-		if (i == (ac - 1))
-		{
-			array[index] = '\n';
-			array[index + 1] = '\0';
-		}
-		else
-		{
-			array[index] = '\n';
-		}
-		index++;
-	}
 	return (array);
 }
