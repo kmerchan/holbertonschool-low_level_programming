27b1441794b9c5fa1ff45985fdce6e4d3c108678
Kelsie Merchant <eislek02@gmail.com>
2020-03-04 02:43:12 +0000

    Add C source code file to define function to free memory space allocated to 2-D array by malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function to free memory allocated to 2-D int array by malloc
+ * @grid: input double pointer to 2-D int array
+ *
+ * @height: input number of rows in 2-D array
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int rows, columns;
+
+	for (rows = 0; rows < height; rows++)
+	{
+		free(grid[rows]);
+	}
+	free(grid);
+}
