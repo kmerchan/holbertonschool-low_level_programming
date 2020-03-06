fa0c8bff7e249b572f195f2b8d0c0dbea3e944f4
Kelsie Merchant <eislek02@gmail.com>
2020-03-05 23:33:02 -0600

    Update code to check if malloc returned null after setting memory for transfer pointer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,6 +35,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	holder = (char *)ptr;
 	transfer = malloc(sizeof(char) * new_size);
+	if (transfer == NULL)
+		return (NULL);
 	for (i = 0; i < old_size; i++)
 		transfer[i] = holder[i];
 	pointer = (void *)transfer;
