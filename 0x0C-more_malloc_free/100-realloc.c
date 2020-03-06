d69a6e856c0406a0556f16468630564d208f5707
Kelsie Merchant <eislek02@gmail.com>
2020-03-05 23:28:19 -0600

    Add C source code file to define function to reallocate memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates block of memory with malloc and free
+ * @ptr: pointer to memory previously allocated
+ *
+ * @old_size: size in bytes of previously allocated memory
+ *
+ * @new_size: size in bytes of new memory block
+ *
+ * Return: void pointer to newly allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *pointer;
+	char *holder;
+	char *transfer;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	holder = (char *)ptr;
+	transfer = malloc(sizeof(char) * new_size);
+	for (i = 0; i < old_size; i++)
+		transfer[i] = holder[i];
+	pointer = (void *)transfer;
+	free(transfer);
+	free(holder);
+	free(ptr);
+	return (pointer);
+}
