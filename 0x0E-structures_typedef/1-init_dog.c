6b90c26b25b606d9be83abb98ce8d27bd377f33f
Kelsie Merchant <eislek02@gmail.com>
2020-03-09 14:18:18 -0500

    Update code to correctly use -> to dereference and initialize variables

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include "dog.h"
+
 /**
  * init_dog - function to initialize variable of type struct dog
  * @d: name of structure defining dog's characteristics
@@ -12,9 +14,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d;
-
-	d.name->name;
-	d.age->age;
-	d.owner->owner
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
