139702af8b89be9273a41b858b828d59552b3ac5
Kelsie Merchant <eislek02@gmail.com>
2020-03-09 13:51:54 -0500

    Add struct description before declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure to talk about dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of dog's owner
+ *
+ * Description: This structure includes information about a dog.
+ */
+
 typedef struct dog
 {
 	char *name;
