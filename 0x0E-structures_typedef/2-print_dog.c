5878590e7439dd3c6e7d2b502022a3a17ae22715
Kelsie Merchant <eislek02@gmail.com>
2020-03-09 14:53:49 -0500

    Add C source code to define function to print information set in struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function to print contents of struct dog
+ * @d: name of structure defining dog's characteristics
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age != 0.0f)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
