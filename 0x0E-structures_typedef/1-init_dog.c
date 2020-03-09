4627b60008caf2b579404b24ac9c6e4fd79ea41b
Kelsie Merchant <eislek02@gmail.com>
2020-03-09 14:10:11 -0500

    Add C source code file to define function to initialize struct dog with information for each variable and update header file to include function prototype

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+/**
+ * init_dog - function to initialize variable of type struct dog
+ * @d: name of structure defining dog's characteristics
+ *
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: name of dog's owner
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d;
+
+	d.name->name;
+	d.age->age;
+	d.owner->owner
+}
