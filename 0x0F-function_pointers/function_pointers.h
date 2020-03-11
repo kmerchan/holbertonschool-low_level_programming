77199daf3b91662b5ffae5b4b0ac1a46800767f3
Kelsie Merchant <eislek@gmail.com>
2020-03-11 19:01:57 +0000

    Update header file to include guard

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 /* declaration of function to use function pointer to print name (0)*/
 void print_name(char *name, void (*f)(char *));
 
@@ -6,3 +9,5 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* declaration of function to search for an integer (2)*/
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
