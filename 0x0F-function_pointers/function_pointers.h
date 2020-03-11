ea1552b2e97227a490754b1509f3ab90841efe6b
Kelsie Merchant <eislek@gmail.com>
2020-03-11 18:21:02 +0000

    Initial commit of header file with initial function prototypes

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+/* declaration of function to use function pointer to print name (0)*/
+void print_name(char *name, void (*f)(char *));
+
+/* declaration of function to execute function for each element of array(1)*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* declaration of function to search for an integer (2)*/
+int int_index(int *array, int size, int (*cmp)(int));
