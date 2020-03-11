0425cec13dc0ef87de0db37d7501bc6d92e7a7e5
Kelsie Merchant <eislek@gmail.com>
2020-03-11 20:22:21 +0000

    Fix typos in function comments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,8 +4,8 @@
 /**
  * array_iterator - executes function as a parameter on each element of array
  * @array: input array of ints to use as function parameters
- * @size: pointer to function with input string
- * @action: pointer to function to fun with each element of array
+ * @size: size of int array
+ * @action: pointer to function to run with each element of array
  *
  */
 
