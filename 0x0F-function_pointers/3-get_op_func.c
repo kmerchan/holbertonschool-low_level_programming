0933e8eb702b69f114dd0fc096535c2028f7a8a0
Kelsie Merchant <eislek02@gmail.com>
2020-03-11 21:01:07 -0500

    Update code to handle condition of (if operator is not one of 5 given) by changing while loop to loop until reaches NULL instead of checking NULL

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,11 @@ int(*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i <= 5)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
