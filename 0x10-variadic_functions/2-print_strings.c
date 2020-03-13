980aac9cb3641ff5ed085faef0d0f09c3beee85e
Kelsie Merchant <eislek@gmail.com>
2020-03-13 23:15:30 +0000

    Add C source code file to define function to print string arguments separated by another input string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - prints all strings passed as arg with separator
+ * @separator: input string to separate printed numbers
+ *
+ * @n: input number of strings
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *string;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+		if (string == NULL)
+			string = "(nil)";
+		if (i != (n - 1))
+			printf("%s%s", string, separator);
+		else
+			printf("%s\n", string);
+	}
+	va_end(args);
+}
