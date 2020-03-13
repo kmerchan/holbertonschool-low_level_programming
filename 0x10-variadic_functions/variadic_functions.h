6abb6347fa36b4c418861af655461267d9db34b5
Kelsie Merchant <eislek02@gmail.com>
2020-03-13 17:09:37 -0500

    Update header file to include new function prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,5 @@
 /* declaration of function to sum all arguments (0)*/
 int sum_them_all(const unsigned int n, ...);
+
+/* declaration of function to print all num args with separator (1)*/
+void print_numbers(const char *separator, const unsigned int n, ...);
