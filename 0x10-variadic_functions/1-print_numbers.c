fa5f04cc3cfda2eea374aefaa9336cb8e6b0a8c2
Kelsie Merchant <eislek02@gmail.com>
2020-03-13 17:09:18 -0500

    Add C source code file to define function to print all number arguments separated by string passed in as separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all numbers passed as arg with separator
+ * @separator: input string to separate printed numbers
+ *
+ * @n: input integers
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d\n", va_arg(args, int));
+	}
+	va_end(args);
+}
