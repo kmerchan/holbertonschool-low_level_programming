d2e535c5f2f7be34f4ce2b92f175ae130ded3a51
Kelsie Merchant <eislek02@gmail.com>
2020-03-14 12:54:13 -0500

    Update code to include check for if number of arguments is less than or equal to 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *string;
 
+	if (n <= 0)
+		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
