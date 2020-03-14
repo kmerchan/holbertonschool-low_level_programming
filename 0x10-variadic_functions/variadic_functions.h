0d3dec1cd75712d77abc7654db4cefd9d2147ad8
Kelsie Merchant <eislek02@gmail.com>
2020-03-13 23:29:10 -0500

    Update header file to include guard

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,6 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
 /* declaration of function to sum all arguments (0)*/
 int sum_them_all(const unsigned int n, ...);
 
@@ -6,3 +9,8 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 /* declaration of function to print all string args with separator (2)*/
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/* declaration of function to print all arguments of varying types (3)*/
+void print_all(const char * const format, ...);
+
+#endif
