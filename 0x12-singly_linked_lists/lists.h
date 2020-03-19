a4982d80cf30d6d10b66f7adfa78d12fab27b4cc
Kelsie Merchant <eislek02@gmail.com>
2020-03-19 14:22:05 -0500

    Add C source code file to define function to print all elements of singly linked list and return number of elements, and add header file with initial struct and prototype

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Included Libraries */
+#include <stdio.h>
+
+/* Structs */
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function Prototypes */
+/* declaration of function to print all elements of a list_t list */
+size_t print_list(const list_t *h);
+
+#endif
