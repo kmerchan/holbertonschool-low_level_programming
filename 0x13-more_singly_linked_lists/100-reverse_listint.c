35c155371435b4c463ca47d28e0b251a6d655c4f
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 16:21:35 -0500

    Add C source code file to define function to reverse order of listint_t list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses order of listint_t list
+ * @head: pointer to input head of singly linked list
+ *
+ * Return: head pointer to beginning of newly reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current, *mover;
+
+	if (head == NULL || (*head) == NULL)
+		return (NULL);
+	current = (*head);
+	if ((*head)->next != NULL)
+		mover = current->next;
+	current->next = NULL;
+	while (mover != NULL)
+	{
+		current = mover;
+		mover = mover->next;
+		current->next = (*head);
+		(*head) = current;
+	}
+	return (*head);
+}
