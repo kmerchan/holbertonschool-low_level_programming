63af2227b8bc1cbd274ee9525231b0f9db3f52a4
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 13:52:37 -0500

    Update code to adjust what points to new node if inserting new node at index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 	new->next = mover->next;
-	mover->next = new;
+	if (idx != 0)
+		mover->next = new;
+	else
+		(*head) = new;
 	return (new);
 }
