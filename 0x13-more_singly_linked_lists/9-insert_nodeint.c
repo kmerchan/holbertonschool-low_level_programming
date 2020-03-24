e19dc6bb96076933188d3487efb595f8ca66f399
Kelsie Merchant <eislek02@gmail.com>
2020-03-23 22:53:26 -0500

    Add C source code to define function to add new node at given index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - adds new node at given index
+ * @head: pointer to input head of singly linked list
+ *
+ * @idx: index to add new node at, starting at 0
+ *
+ * @n: input data to add for new node
+ *
+ * Return: pointer to new node or NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *mover = (*head);
+	unsigned int i;
+
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < idx; i++)
+	{
+		if (mover == NULL)
+			return (NULL);
+		if (i != (idx - 1))
+			mover = mover->next;
+	}
+	new->n = n;
+	new->next = mover->next;
+	mover->next = new;
+	return (new);
+}
