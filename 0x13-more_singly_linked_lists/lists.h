b7ae0f324be6ead8209b0c2ed1289df062d69d49
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 00:48:54 -0500

    Update header file to include new function prototype to delete node at given index

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -51,4 +51,7 @@ int sum_listint(listint_t *head);
 /*declaration of function to add new node at given index in listint_t list (9)*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
+/*declaration of function to delete node at given index in listint_t list (10)*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
