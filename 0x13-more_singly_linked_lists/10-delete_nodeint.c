d4137b244628ea37ad7905aa9d63df3039d4304d
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 00:47:50 -0500

    Add C source code file to dedefine function to delete node at given index and change previous pointer to equal the next node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at given index
+ * @head: pointer to input head of singly linked list
+ *
+ * @idx: index to add new node at, starting at 0
+ *
+ * Return: 1 if successful, -1 if failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *previous, *index_node;
+	listint_t *mover = (*head);
+	unsigned int i;
+
+
+	for (i = 0; i <= index; i++)
+	{
+		if (mover == NULL)
+			return (-1);
+		if (i == (index - 1))
+			previous = mover;
+		if (i == index)
+			index_node = mover;
+		mover = mover->next;
+	}
+	if (index != 0)
+		previous->next = mover;
+	else
+		*head = mover;
+	free(index_node);
+	return (1);
+}
