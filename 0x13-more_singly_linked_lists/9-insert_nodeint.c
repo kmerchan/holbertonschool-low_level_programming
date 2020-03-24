31dcf6c4c07f0b02f20345ba31913a98698abb68
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 13:46:04 -0500

    Update code to initialize mover pointer after checking if head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,11 +14,12 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *mover = (*head);
+	listint_t *mover;
 	unsigned int i;
 
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
+	mover = (*head);
 	for (i = 0; i < idx; i++)
 	{
 		if (mover == NULL)
