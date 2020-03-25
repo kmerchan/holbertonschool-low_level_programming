892cf64c4dc002b94238860d8f12ec71fd2dfa98
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 23:24:57 -0500

    Add C source code file to define function to find the start of loop in listint_t list or return NULL if no loop exists

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the start of a loop of a listint_t list
+ * @head: input head of singly linked list to print
+ *
+ * Return: pointer to location of loop start or NULL if no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	while (head != NULL)
+	{
+		if (head->next >= head)
+		{
+			head = head->next;
+			return (head);
+		}
+		head = head->next;
+	}
+	return (NULL);
+}
