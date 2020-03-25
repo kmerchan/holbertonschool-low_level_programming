3133b44e614b77bbf4fe1e8578b215517a7b73bb
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 21:48:51 -0500

    Update C source code file to declare size of array to store node addresses in to check if at node previously seen as looping through to print each node's data

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,15 +11,13 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t n = 0;
 	size_t i = 0, j = 0;
-	(int *) *array;
+	int *array[999];
 
-	if (array == NULL)
-		exit(98);
 	while (head != NULL)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (head == array[i])
+			if ((int *)head == array[i])
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				exit(98);
@@ -27,16 +25,9 @@ size_t print_listint_safe(const listint_t *head)
 		}
 		n++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		array = malloc(sizeof(int *));
-		if (array == NULL)
-		{
-			free(array);
-			exit(98);
-		}
-		array[j] = head;
+		array[j] = (int *)head;
 		j++;
 		head = head->next;
 	}
-	free(array);
 	return (n);
 }
