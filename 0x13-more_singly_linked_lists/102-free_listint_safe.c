ee1bc493bad7c7399da05de17cad77a7d434dc58
Kelsie Merchant <eislek02@gmail.com>
2020-03-24 23:04:59 -0500

    Add C source code file to define function to free all nodes of listint_t list, checking for lists that loop back on themselves

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees all the nodes of a listint_t list
+ * @h: pointer to input head of singly linked list
+ *
+ * Return: Number of nodes
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t n = 0;
+	size_t i = 0, j = 0;
+	listint_t *tmp;
+	listint_t *array[9999];
+
+	if (h == NULL || (*h) == NULL)
+		return (0);
+	while ((*h) != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if ((*h) == array[i])
+			{
+				free(*h);
+				(*h) = NULL;
+				return (n);
+			}
+		}
+		n++;
+		array[j] = (*h);
+		j++;
+		tmp = (*h);
+		(*h) = (*h)->next;
+		free(tmp);
+	}
+	(*h) = NULL;
+	return (n);
+}
