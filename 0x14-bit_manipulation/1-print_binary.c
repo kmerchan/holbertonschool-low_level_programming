b1e207ca017382fa23ec5e0c76a6704b5d1b7324
Kelsie Merchant <eislek02@gmail.com>
2020-03-26 13:36:28 -0500

    Add C source code file to define function to print binary representation of unsigned int

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function to print binary representation of unsigned int
+ * @n: input unsigned int to print binary conversion of
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int count = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (power2(count) <= n)
+		count++;
+	count--;
+	for (; count >= 0; count--)
+	{
+		if (power2(count) > n)
+			_putchar('0');
+		else
+		{
+			_putchar('1');
+			n -= power2(count);
+		}
+	}
+}
+
+/**
+ * power2 - determines power of two to compare to unsigned int
+ * @count: input count of power value currently on
+ *
+ * Return: integer value of power, 2^count
+ */
+
+unsigned long int power2(int count)
+{
+	unsigned long int m = 1;
+	int i = 0;
+
+	for (i = 0; i < count; i++)
+		m *= 2;
+	return (m);
+}
