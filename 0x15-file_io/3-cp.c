e3f91750784a905dd247b002f28d1b4402b0de02
Kelsie Merchant <eislek02@gmail.com>
2020-03-31 18:38:55 -0500

    Update code to include more error checks if read or write fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,10 @@
 #include <fcntl.h>
 #define STDERR STDERR_FILENO
 
+void cant_read(char *s);
+void cant_write(char *s);
+void cant_close(int fd);
+
 /**
  * main - function to copy contents of one file to another
  * @argc: int count of how many arguments were passed to function
@@ -14,7 +18,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2, sz1;
+	int fd1, fd2;
+	ssize_t sz1, sz2;
 	char *buf = malloc(sizeof(char) * 1024);
 
 	if (argc != 3)
@@ -25,31 +30,60 @@ int main(int argc, char *argv[])
 
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 < 0 || buf == NULL)
-	{
-		dprintf(STDERR, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+		cant_read(argv[1]);
 	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd2 < 0)
-	{
-		dprintf(STDERR, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		cant_write(argv[2]);
 
 	sz1 = read(fd1, buf, 1024);
-	write(fd2, buf, sz1);
+	if (sz1 < 0)
+		cant_read(argv[1]);
+
+	sz2 = write(fd2, buf, sz1);
+	if (sz2 < 0)
+		cant_write(argv[2]);
 
 	free(buf);
 	if (close(fd1) < 0)
-	{
-		dprintf(STDERR, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		cant_close(fd1);
 	if (close(fd2) < 0)
-	{
-		dprintf(STDERR, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
+		cant_close(fd2);
+
 	return (0);
+}
+
+/**
+ * cant_read - prints error message and exits if file_from can't be read
+ * @s: input string referring to file_from name
+ *
+ */
+
+void cant_read(char *s)
+{
+	dprintf(STDERR, "Error: Can't read from file %s\n", s);
+	exit(98);
+}
+
+/**
+ * cant_write - prints error message and exits if can't write to file_to
+ * @s: input string referring to file_to name
+ *
+ */
+
+void cant_write(char *s)
+{
+	dprintf(STDERR, "Error: Can't write to %s\n", s);
+	exit(99);
+}
+
+/**
+ * cant_close - prints error message and exits if cannot close file
+ * @fd: input int value of file
+ *
+ */
+
+void cant_close(int fd)
+{
+	dprintf(STDERR, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
