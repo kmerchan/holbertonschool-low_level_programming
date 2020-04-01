57920043ea4e9f5ebd181e2cbd14dab792f6b1eb
Kelsie Merchant <eislek02@gmail.com>
2020-03-31 21:16:50 -0500

    Update code to call write only when opening file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 < 0)
 		cant_read(argv[1]);
-	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 < 0)
 		cant_write(argv[2]);
 
