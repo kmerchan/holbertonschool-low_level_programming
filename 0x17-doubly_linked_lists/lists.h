cb5c8d3f942cc070e424cdca5ddac487de49dfd8
Kelsie Merchant <eislek02@gmail.com>
2020-05-01 16:14:46 -0500

    Add C source code file to define function to sum data from all elements of a dlistint_t list and update header file to include new function prototype

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,22 +23,25 @@ typedef struct dlistint_s
 } dlistint_t;
 
 /* FUNCTION PROTOTYPES */
-/* declaration of function to print all elements of a doubly linked list */
+/* declaration of function to print all elements of a doubly linked list (0)*/
 size_t print_dlistint(const dlistint_t *h);
 
-/* declaration of funtion to return number of nodes of a doubly linked list */
+/* declaration of funtion to return number of nodes in doubly linked list (1)*/
 size_t dlistint_len(const dlistint_t *h);
 
-/* declaration of function to add new node to start of doubly linked list */
+/* declaration of function to add new node to start of doubly linked list (2)*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
-/* declaration of function to add new node to end of doubly linked list */
+/* declaration of function to add new node to end of doubly linked list (3)*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
-/* declaration of function to free all elements of a doubly linked list */
+/* declaration of function to free all elements of a doubly linked list (4)*/
 void free_dlistint(dlistint_t *head);
 
-/* declaration of function to return the nth node of a doubly linked list */
+/* declaration of function to return the nth node of a doubly linked list (5)*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* declaration of function to sum data from elements of dlistint_t list (6)*/
+int sum_dlistint(dlistint_t *head);
 
 #endif
