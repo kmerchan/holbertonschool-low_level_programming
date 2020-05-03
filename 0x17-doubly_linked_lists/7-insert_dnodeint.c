679a78c413263531d9c46b1eeb596457cee0e14a
Kelsie Merchant <eislek02@gmail.com>
2020-05-03 14:26:13 -0500

    Update C function code to check if *head pointer is NULL before dereferencing if index is 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		newnode->next = (*h);
-		(*h)->prev = newnode;
+		if ((*h) != NULL)
+			(*h)->prev = newnode;
 		*h = newnode;
 		return (newnode);
 	}
