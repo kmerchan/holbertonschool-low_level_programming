998f60e37568ce30c5134c1d07ca7d2d8a6301cb
Kelsie Merchant <eislek02@gmail.com>
2020-05-29 00:33:15 -0500

    Add test of new function to add new key/value element to hash table

---
+++ 0x1A-hash_tables/test_files/3-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+	unsigned long int index;
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "betty", "holberton");
+	index = (hash_djb2((unsigned char *)"betty") % 1024);
+	printf("key: %s\n", ht->array[index]->key);
+	printf("value: %s\n", ht->array[index]->value);
+	printf("-----Reset key-----\n");
+	hash_table_set(ht, "betty", "reset");
+	index = (hash_djb2((unsigned char *)"betty") % 1024);
+	printf("key: %s\n", ht->array[index]->key);
+	printf("value: %s\n", ht->array[index]->value);
+	printf("-----1 collision-----\n");
+	hash_table_set(ht, "stylist", "value1");
+	hash_table_set(ht, "subgenera", "value2");
+	index = (hash_djb2((unsigned char *)"stylist") % 1024);
+	printf("key: %s\n", ht->array[index]->key);
+	printf("value: %s\n", ht->array[index]->value);
+	printf("key: %s\n", ht->array[index]->next->key);
+	printf("value: %s\n", ht->array[index]->next->value);
+	return (EXIT_SUCCESS);
+}
