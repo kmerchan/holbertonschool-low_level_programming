04bcfb3f70f7aaab01a2d572682ee4b3ed66d195
Kelsie Merchant <eislek02@gmail.com>
2020-07-07 23:26:30 -0500

    Remove passes if first or last row

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,8 +11,6 @@ def island_perimeter(grid):
     """
     perimeter = 0
     for rows in range(len(grid)):
-        if rows == 0 or rows == ((len(grid)) - 1):
-            continue
         for columns in range(len(grid[rows])):
             if grid[rows][columns] == 1:
                 if grid[rows][columns - 1] == 0:
