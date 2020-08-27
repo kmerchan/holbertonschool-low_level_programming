06b36c236e19e2a8c6e174284c7efa8228783ef2
Kelsie Merchant <eislek02@gmail.com>
2020-08-27 00:45:45 -0500

    Add comments for all tests

--- 0x1E-search_algorithms/test_files/104-main.c
+++ 0x1E-search_algorithms/test_files/104-main.c
@@ -12,6 +12,9 @@ int main(void)
 	int array[] = {
 		0, 1, 2, 5, 5, 6, 6, 7, 8, 9
 	};
+	int arr2[] = {
+		0, 1, 2, 4, 5, 6, 6, 7, 8, 9
+	};
 	int arr[] = {
 		0, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 8, 9, 9
 	};
@@ -21,17 +24,31 @@ int main(void)
 	size_t size = sizeof(array) / sizeof(array[0]);
 	size_t sz = sizeof(arr) / sizeof(arr[0]);
 
-	printf("Found %d at index: %d\n\n", 6, advanced_binary(NULL, sz, 3));
+	/* test if NULL array passed to function */
+	printf("Found %d at index: %d\n\n", 6, advanced_binary(NULL, size, 3));
+	/* test if size of array is 0 */
+	printf("Found %d at index: %d\n\n", 6, advanced_binary(array, 0, 3));
+	/* test is array is only 1 element long, and matches */
 	printf("Found %d at index: %d\n\n", 3, advanced_binary(a, 1, 3));
+	/* test is array is only 1 element long, and isn't a match */
 	printf("Found %d at index: %d\n\n", 3, advanced_binary(a, 1, -3));
-
+	/* first test given on project page, finds non-repeat match */
 	printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
+	/* second test given on project page, finds first repeated match */
 	printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
+	/* third test given on project page, shows no match found, non-match beyond array max */
 	printf("Found %d at index: %d\n\n", 999, advanced_binary(array, size, 999));
 
+	/* tests if match is found, but not at index 0 so there could be a previous match */
+	printf("Found %d at index: %d\n\n", 5, advanced_binary(arr2, size, 5));
+
+	/* tests multiple repeats in upper half of array */
 	printf("Found %d at index: %d\n\n", 6, advanced_binary(arr, sz, 6));
+	/* tests for non-match in middle of array */
 	printf("Found %d at index: %d\n\n", 7, advanced_binary(arr, sz, 7));
+	/* tests multiple repeats in lower half of array */
 	printf("Found %d at index: %d\n\n", 1, advanced_binary(arr, sz, 1));
+	/* tests for non-match beyond array min */
 	printf("Found %d at index: %d\n\n", -3, advanced_binary(arr, sz, -3));
 	return (EXIT_SUCCESS);
 }
