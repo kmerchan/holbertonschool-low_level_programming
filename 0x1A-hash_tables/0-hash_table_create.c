77efdc31b1a8a6fae6a86630d0884f4741c80e11
Kelsie Merchant <eislek02@gmail.com>
2021-07-25 14:00:08 -0500

    Testing peer's code against checker for student tutoring

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function to create hash table
- * @size: size of the array
- *
- * Return: pointer to newly created hash table, or NULL if failed
+ * *hash_table_create - prototype func
+ * @size: is size of the array
+ * Return: pointer to the new hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = NULL;
-	unsigned long int i = 0;
+	unsigned long int h = 0;
+	hash_table_t *NewTable = NULL;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	NewTable = malloc(sizeof(hash_table_t));
+
+	if (NewTable == NULL)
 		return (NULL);
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-		free(ht);
+
+	NewTable->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (NewTable->array == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		ht->array[i] = NULL;
-	}
-	return (ht);
+
+	NewTable->size = size;
+
+	for (h = 0; h < size; h++)
+		NewTable->array[h] = NULL;
+
+	return (NewTable);
+
 }
